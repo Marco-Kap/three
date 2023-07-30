c15884ac47ef3340faf05d2e83c2741c41ec58cc
Marco-Kap <kikakeks@gmx.net>
2023-07-30 22:43:58 +0200

    minor changes

--- game.ts
+++ game.ts
@@ -16,6 +16,7 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(70, WIDTH / HEIGHT);
 camera.position.z = 50;
 
+scene.background = new THREE.Color(0x1c28cc);
 scene.add(camera);
 
 /*
@@ -28,7 +29,7 @@ const cube = new THREE.Mesh(boxGeometry, basicMaterial);
 
 scene.add(cube);
 cube.rotation.set(0.2, 1, 0);
-cube.position.x = -25; 
+cube.position.x = -25;
 
 //same procedure for a torus
 const torusGeometry = new THREE.TorusGeometry(7, 1, 6, 12);
@@ -43,6 +44,7 @@ const lambertMaterial = new THREE.MeshLambertMaterial({
 });
 const dodecahedron = new THREE.Mesh(dodecahedronGeometry, lambertMaterial);
 dodecahedron.position.x = 0;
+dodecahedron.castShadow;
 scene.add(dodecahedron);
 
 //adds light to the scene
@@ -55,6 +57,28 @@ const alight = new THREE.AmbientLight(0x4040);
 alight.position.set(10, 15, 50);
 scene.add(alight);
 
+function controls() {
+  addEventListener("keydown", (e) => {
+    switch (e.code) {
+      case "ArrowDown":
+        dodecahedron.position.y = dodecahedron.position.y - 0.008;
+        break;
+      case "ArrowUp":
+        dodecahedron.position.y = dodecahedron.position.y + 0.008;
+        break;
+      case "ArrowLeft":
+        dodecahedron.position.x = dodecahedron.position.x - 0.008;
+        break;
+      case "ArrowRight":
+        dodecahedron.position.x = dodecahedron.position.x + 0.008;
+        break;
+      default:
+        dodecahedron.position.x = dodecahedron.position.x;
+    }
+    console.log(e.code);
+  });
+}
+
 let t = 0;
 //this is where the magic happens requestAnimationFrame() is called on a loop
 //call this function after all the objects are initialized
@@ -62,7 +86,8 @@ function render() {
   requestAnimationFrame(render);
   renderer.render(scene, camera);
 
-  dodecahedron.position.y = -7 * Math.sin(t * 2);
-  t += 0.005;
+  controls();
 }
+//dodecahedron.position.y = -7 * Math.sin(t * 2);
+t += 0.001;
 render();
